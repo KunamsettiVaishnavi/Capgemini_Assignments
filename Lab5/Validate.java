aeec111db8eee2912374ca8ab7e8acbe9e9cd015
KunamsettiVaishnavi <71434509+KunamsettiVaishnavi@users.noreply.github.com>
2021-04-18 22:22:04 +0530

    Create Validate.java

---
+++ Lab5/Validate.java
@@ -0,0 +1,14 @@
+package Lab5;
+
+public class Validate {
+	public void checkAge(int age) throws ValidAgeException, InvalidAgeException{
+		if(age < 15) {
+			InvalidAgeException invalid = new InvalidAgeException("INVALID AGE");
+			throw(invalid);
+		}else {
+			ValidAgeException valid = new ValidAgeException("Valid Age");
+			throw valid;
+		}
+	}
+
+}
